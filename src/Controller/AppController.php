3aae448111867321cb759eb91cbcc790ede5fb80
ValantineD <v.dagany@gmail.com>
2026-02-13 16:31:58 +0100

    products on home page

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +11,18 @@ use Symfony\Component\Routing\Attribute\Route;
 class AppController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function home(): Response
+    public function home(ProductRepository $productRepository): Response
     {
-        return $this->render('home.html.twig');
+        return $this->render('home.html.twig', [
+            'products' => $productRepository->findAll()
+        ]);
+    }
+
+    #[Route('/product/{id}', name: 'app_public_product_show', methods: ['GET'])]
+    public function product_show(Product $product): Response
+    {
+        return $this->render('product_show.html.twig', [
+            'product' => $product
+        ]);
     }
 }
